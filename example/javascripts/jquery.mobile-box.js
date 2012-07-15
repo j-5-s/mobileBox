65ac71247b1942da40d803c4b722f183f82c3c8e
James Charlesworth <james.charlesworth@gmail.com>
2012-07-15 14:34:57 -0400

    fixed vertical centering of portait images

--- example/javascripts/jquery.mobile-box.js
+++ example/javascripts/jquery.mobile-box.js
@@ -154,11 +154,8 @@
 					layout = 'portrait';
 				}
 
-				var vertical_spacing = screenHeight - target.height;
+				var h = target.height;
 
-
-				vertical_spacing = (vertical_spacing >= 0) ? ((vertical_spacing/4)) : 0;
-					
 				//need to set the css width so it does not stretch
 				if ( target.width < screenWidth ) {
 					$(img).css({width: target.width + 'px'});
@@ -169,10 +166,15 @@
 					//calc the spacing
 					var r = target.width/ target.height,
 						s = (r * (screenHeight) ) / 2;
-
-					$(img).css({'left':s + 'px'});
-
+					//use margin-left as opposed to left because otherwise it does not slide	
+					$(img).css({'margin-left':s + 'px'});
+					//40 for the header
+					h = r * h + 40;
 				}
+				
+				var vertical_spacing = screenHeight -  h;
+
+				vertical_spacing = (vertical_spacing >= 0) ? ((vertical_spacing/4)) : 0;
 
 				$(img).css({'margin-top': vertical_spacing +'px'} );
 
@@ -247,12 +249,14 @@
 							var r = width / height;
 							horizontal_spacing = (r * (screenHeight) ) / 2;
 						}
+						//alert(horizontal_spacing)
 
 						if (increment > 0) {
 							$(this).css( {right:'-'+screenWidth + 'px'} );
 							animateObj.right = horizontal_spacing + 'px';
 						} else {
 							$(this).css( {left:'-'+screenWidth + 'px'} );
+
 							animateObj.left = horizontal_spacing+'px';
 						}
 						$('.mobileBoxTitle').html( $nextImage.attr('title') );
